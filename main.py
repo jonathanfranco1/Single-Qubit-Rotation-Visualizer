8e96323b28b10b4e184bc7c46dbcba35479626f1
Jonathan Franco <97241584+jonathanfranco1@users.noreply.github.com>
2023-02-15 02:01:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ import qiskit
 from qiskit import QuantumCircuit
 from qiskit.visualization import visualize_transition
 
-# Ignore unnecessary warnings
+
 warnings.simplefilter("ignore")
 
-# Define Window
+
 root = tkinter.Tk()
-root.title('Quantum Visualizer')
+root.title('Single Qubit Rotation')
 
-# set the icon
+
 root.geometry('399x410')
-root.resizable(0,0) # Blocking the resizing feature
+root.resizable(0,0) 
 
-# Define the colors and fonts
 background = '#2c94c8'
 buttons = '#834558'
 special_buttons = '#bc3454'
 button_font = ('Arial', 18)
 display_font = ('Arial', 32)
 
-# Initialize the Quantum Circuit
+
 def initialize_circuit():
     global circuit
     circuit = QuantumCircuit(1)
@@ -34,18 +33,16 @@ initialize_circuit()
 
 theta = 0
 
-# Define Functions
-# Define functions for non-qiskit buttons
+
 def display_gate(gate_input):
     """
     Adds a corresponding gate notation in the display to track the operations.
     If the number of operation reach ten, all gate buttons are disabled.
     """
-    # Insert the defined gate
+    
     display.insert(END,gate_input)
 
-    # Check if the number of operations has reached ten, if yes,
-    # disable all the gate buttons
+   
     input_gates = display.get()
     num_gates_pressed = len(input_gates)
     list_input_gates = list(input_gates)
@@ -63,13 +60,13 @@ def clear(circuit):
     Reintializes the Quantum Circuit for fresh calculation!
     Checks if the gate buttons are disabled, if so, enables the buttons
     """
-    # clear the display
+
     display.delete(0, END)
 
-    # reset the circuit to initial state |0> 
+
     initialize_circuit()
 
-    # Checks if the buttons are disabled and if so, enables them
+
     if x_gate['state']==DISABLED:
         gates = [x_gate, y_gate, z_gate, Rx_gate, Ry_gate, Rz_gate, s_gate, sd_gate, t_gate, td_gate, hadamard]
         for gate in gates:
@@ -86,7 +83,7 @@ def about():
 
     text = tkinter.Text(info, height = 20, width = 20)
 
-    # Create label
+
     label = tkinter.Label(info, text = "About Quantum Glasses:")
     label.config(font =("Arial", 14))
     
@@ -123,10 +120,10 @@ def about():
     label.pack()
     text.pack(fill='both',expand=True)
 
-    # Insert the text
+
     text.insert(END,text_to_display)
 
-    # run
+
     info.mainloop()
 
 
@@ -153,7 +150,6 @@ def user_input(circuit,key):
     Rotation gates Rx, Ry, Rz.
     """
 
-    # Initialize and define the properties of window
     get_input = tkinter.Tk()
     get_input.title('Get Theta')
     get_input.geometry('360x160')
@@ -196,8 +192,7 @@ def user_input(circuit,key):
 
     get_input.mainloop()
 
-# Define functions for qiskit-based buttons
-# Define the function for visualize button
+
 def visualize_circuit(circuit,window):
     """
     Visualizes the single qubit rotations corresponding to applied gates in a separate tkinter window.
@@ -208,20 +203,17 @@ def visualize_circuit(circuit,window):
     except qiskit.visualization.exceptions.VisualizationError:
         window.destroy()
 
-# Define Layout
-# Define the Frames
 display_frame = tkinter.LabelFrame(root)
 button_frame = tkinter.LabelFrame(root,bg='black')
 display_frame.pack()
 button_frame.pack(fill='both',expand=True)
 
-# Define the Display Frame Layout
+
 display = tkinter.Entry(display_frame, width=120, font=display_font, bg=background, borderwidth=2, justify=LEFT)
 display.pack(padx=3,pady=4)
 
-# Define the Button Frame Layout
 
-# Define the first row of buttons
+
 x_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='X',command=lambda:[display_gate('x'),circuit.x(0)])
 y_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='Y',command=lambda:[display_gate('y'),circuit.y(0)])
 z_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='Z',command=lambda:[display_gate('z'),circuit.z(0)])
@@ -229,7 +221,6 @@ x_gate.grid(row=0,column=0,ipadx=45, pady=1)
 y_gate.grid(row=0,column=1,ipadx=45, pady=1)
 z_gate.grid(row=0,column=2,ipadx=53, pady=1, sticky='E')
 
-# Define the second row of buttons
 Rx_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RX',command=lambda:[display_gate('Rx'),user_input(circuit,'x')])
 Ry_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RY',command=lambda:[display_gate('Ry'),user_input(circuit,'y')])
 Rz_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RZ',command=lambda:[display_gate('Rz'),user_input(circuit,'z')])
@@ -237,7 +228,7 @@ Rx_gate.grid(row=1,column=0,columnspan=1,sticky='WE', pady=1)
 Ry_gate.grid(row=1,column=1,columnspan=1,sticky='WE', pady=1)
 Rz_gate.grid(row=1,column=2,columnspan=1,sticky='WE', pady=1)
 
-# Define the third row of buttons
+
 s_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='S',command=lambda:[display_gate('s'),circuit.s(0)])
 sd_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='SD',command=lambda:[display_gate('SD'),circuit.sdg(0)])
 hadamard = tkinter.Button(button_frame, font=button_font, bg=buttons, text='H',command=lambda:[display_gate('H'),circuit.h(0)])
@@ -245,26 +236,24 @@ s_gate.grid(row=2,column=0,columnspan=1,sticky='WE', pady=1)
 sd_gate.grid(row=2,column=1,sticky='WE', pady=1)
 hadamard.grid(row=2, column=2, rowspan=2,sticky='WENS', pady=1)
 
-# Define the fifth row of buttons
 t_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='T', command=lambda:[display_gate('t'),circuit.t(0)])
 td_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='TD',command=lambda:[display_gate('TD'),circuit.tdg(0)])
 t_gate.grid(row=3,column=0,sticky='WE', pady=1)
 td_gate.grid(row=3,column=1,sticky='WE', pady=1)
 
-# Define the Quit and Visualize buttons
+
 quit = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Quit',command=root.destroy)
 visualize = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Visualize',command=lambda:visualize_circuit(circuit,root))
 quit.grid(row=4,column=0,columnspan=2,sticky='WE',ipadx=5, pady=1)
 visualize.grid(row=4,column=2,columnspan=1,sticky='WE',ipadx=8, pady=1)
 
-# Define the clear button
+
 clear_button = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Clear',command=lambda:clear(circuit))
 clear_button.grid(row=5,column=0,columnspan=3,sticky='WE')
 
-# Define the about button
+
 about_button = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='About',command=about)
 about_button.grid(row=6,column=0,columnspan=3,sticky='WE')
 
-# Run the main loop
 root.mainloop()
 
